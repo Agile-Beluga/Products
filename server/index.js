59ce4b8baaa8e351f139c36751620c3e8ecc8666
Elaine <eyw2008@gmail.com>
2020-01-10 19:17:08 -0500

    remove docker compose files

--- server/index.js
+++ server/index.js
@@ -11,11 +11,22 @@ app.use(bodyParser.urlencoded({
     extended: true,
 }))
 const redis = require("redis"),
-    cache = redis.createClient({
+    client = redis.createClient({
         host: 'redis'
     });
-cache.on("error", function (err) {
+client.on("error", function (err) {
     console.log("Error " + err);
+});
+
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+    client.quit();
 });
 // app.get('/loaderio-a9b96e566e83ea103824b022d25a1116', (request, response) => {
 //     response.send('loaderio-a9b96e566e83ea103824b022d25a1116')
